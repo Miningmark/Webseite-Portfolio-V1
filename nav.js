d7f6c59d0cbc398428fe417fa6630e9221d94f31
Markus <markus.sibbe@t-online.de>
2024-02-04 13:50:58 +0100

    add js for navigation

---
+++ nav.js
@@ -0,0 +1,17 @@
+
+let introHeight = window.innerHeight * 0.75;
+
+window.addEventListener('scroll', function(){
+    const currentScrollPos = window.scrollY;
+    console.log(introHeight);
+    if(introHeight < currentScrollPos){
+        this.document.querySelector('.nav-container').classList.add('show');
+    }else{
+        this.document.querySelector('.nav-container').classList.remove('show');
+    }
+    prevScrollPos = currentScrollPos;
+});
+
+
+
+
