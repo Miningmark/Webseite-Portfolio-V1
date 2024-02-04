8599762c004654db0e9dd211bbb9a4eb67e09804
Markus <markus.sibbe@t-online.de>
2024-02-05 00:22:24 +0100

    add color change at Menu text

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
-
+//Show Menu after Scroll 75% VH down from Sitetop
 let introHeight = window.innerHeight * 0.75;
 
 window.addEventListener('scroll', function(){
     const currentScrollPos = window.scrollY;
-    console.log(introHeight);
+    //console.log(introHeight);
     if(introHeight < currentScrollPos){
         this.document.querySelector('.nav-container').classList.add('show');
     }else{
@@ -11,3 +11,62 @@ window.addEventListener('scroll', function(){
     }
     prevScrollPos = currentScrollPos;
 });
+
+
+var flag = false;
+var sections = document.querySelectorAll('.scroll-section');
+
+window.addEventListener('scroll', function () {
+    if(!flag){
+        sections = document.querySelectorAll('.scroll-section');
+    }
+    var maxVisibleSection = getMaxVisibleSection();
+    updateActiveMenu(maxVisibleSection);
+    //var rect = this.document.querySelector('.testtest').getBoundingClientRect();
+});
+
+function getMaxVisibleSection() {
+    var maxVisibility = 0;
+    var maxVisibleSection = null;
+    sections.forEach(function (section) {
+        var visibility = getVisibility(section);
+
+        if (visibility > maxVisibility) {
+            maxVisibility = visibility;
+            maxVisibleSection = section;
+        }
+    });
+    //console.log(maxVisibleSection);
+    //console.log(maxVisibleSection);
+    return maxVisibleSection;
+}
+
+function getVisibility(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+
+    var visibleHeight = Math.min(rect.bottom, html.clientHeight) - Math.max(rect.top, 0);
+
+    //console.log(element);
+    //console.log(visibleHeight);
+    return  visibleHeight;  
+}
+
+function updateActiveMenu(activeSection) {
+
+    sections.forEach(function (section) {
+        var iconId = section.dataset.icon;
+        var iconElement = document.querySelector('#icon-' + iconId);
+        var textElement = document.querySelector('#text-' + iconId);
+        //console.log(iconId);
+        //console.log(iconElement);
+
+        if (section === activeSection) {
+            iconElement.classList.add('menu-active');
+            textElement.classList.add('menu-active');
+        } else {
+            iconElement.classList.remove('menu-active');
+            textElement.classList.remove('menu-active');
+        }
+    });
+}
